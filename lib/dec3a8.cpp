9d352e051ef8129318c22fd8ebb7a2f2f99c78c9
mateo <gamboamateo14@gmail.com>
2025-04-16 16:12:57 -0300

    correccion_0

--- lib/dec3a8.cpp
+++ lib/dec3a8.cpp
@@ -1,5 +1,5 @@
 #include "dec3a8.h"
-int valor;
+int valor, i = 0;
 void dec3a8_init()
 {
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
@@ -13,15 +13,88 @@ void dec3a8(int I0, int I1, int I2)
 
     switch (valor)
     {
-        case 000;
-            GPIOB->BSRR|=0x00;
-            GPIOB->BSRR|=(1<<0);
-        case 001;
-            GPIOB->BSRR|=0x00;
-            GPIOB->BSRR|=(1<<1);
-        case 010;
-            GPIOB->BSRR|=0x00;
-            GPIOB->BSRR|=(1<<2);
-        case 011;
+    case 000:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 0)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 0);
+        break;
+    case 001:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 1)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 1);
+        break;
+    case 010:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 2)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 2);
+        break;
+    case 011:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 3)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 3);
+        break;
+    case 100:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 4)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 4);
+        break;
+    case 101:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 5)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR = (1 << 5);
+        break;
+    case 110:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 6)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 6);
+        break;
+    case 111:
+        for (i = 0; i < 8; i++)
+        {
+            if (i != 7)
+            {
+                GPIOB->BSRR |= (1 << (i + 16));
+            }
+        }
+        GPIOB->BSRR |= (1 << 7);
+        break;
+    default:
+        GPIOB->BSRR |= 0x00;
+        break;
     }
 }
---
+++ lib/dec3a8.h
@@ -0,0 +1,10 @@
+#ifndef DEC3A8_H
+#define DEC3A8_H
+
+#include "Arduino.h"
+
+void dec3a8_init();
+
+void dec3a8(int, int, int);
+
+#endif
